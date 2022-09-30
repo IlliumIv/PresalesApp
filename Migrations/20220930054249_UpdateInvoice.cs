711734d3b724fb7ca267fcee84fd77835b84d1ad
IlliumIv <alm.sparta.ao@gmail.com>
2022-09-30 10:54:32 +0500

    Update invoice, add migrations

---
+++ Migrations/20220930054249_UpdateInvoice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PresalesStatistic.Migrations
+{
+    public partial class UpdateInvoice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastPay",
+                table: "Invoices",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastShipment",
+                table: "Invoices",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastPay",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "LastShipment",
+                table: "Invoices");
+        }
+    }
+}
