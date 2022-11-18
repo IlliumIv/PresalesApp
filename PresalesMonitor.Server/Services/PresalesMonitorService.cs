2ad331bd6599e3b631006f7b31286c65554e8ab6
IlliumIv <alm.sparta.ao@gmail.com>
2022-11-18 18:21:32 +0500

    Test receive data

---
+++ PresalesMonitor.Server/Services/PresalesMonitorService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using Google.Protobuf.WellKnownTypes;
+using PresalesMonitor;
+using PresalesMonitor.Shared;
+
+namespace PresalesMonitor.Server.Services
+{
+    public class PresalesMonitorService : Presales.PresalesBase
+    {
+        public override Task<PresaleReply> GetStats(Presale request, ServerCallContext context)
+        {
+            var reply = new PresaleReply();
+            var rng = new Random();
+            var names = PresalesMonitor.Program.GetPresalesNames();
+
+            reply.Presales.Add(names.Select(index => new Presale
+            {
+                Name = index
+            }));
+
+            return Task.FromResult(reply);
+        }
+    }
+}
