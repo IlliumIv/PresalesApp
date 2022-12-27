92cdd12fb90f7875c4777348bba58bf09df7786f
IlliumIv <alm.sparta.ao@gmail.com>
2022-12-27 13:18:33 +0500

    Fixes
    + Add presale status
    + Add presales filter to API
    + Add image orientation parameter to API
    + Allow select custom period on monitor page
    + Add numeration to KPI file
    = Change base css for input and select forms
    = Change form of period select on KPI page
    = Fix wrong work time calculation
    = Fix numeration on KPI page
    = Fix name of KPI file

---
+++ PresalesMonitor.Entities/Migrations/20221220082843_AddPresaleStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PresalesMonitor.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPresaleStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Presales",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Presales");
+        }
+    }
+}
