3a6a02f578af7995c4973949e363dd53a679c10a
Illium <alm.sparta.ao@gmail.com>
2023-10-30 13:30:27 +0500

    Rename Helpers class to make methods shorter

--- PresalesApp.Web/Client/Pages/PresalesDashboard.razor.cs
+++ PresalesApp.Web/Client/Pages/PresalesDashboard.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using PresalesApp.Web.Client.Views;
+using PresalesApp.Web.Client.Helpers;
 using PresalesApp.Web.Shared;
 using Department = PresalesApp.Web.Shared.Department;
 using Position = PresalesApp.Web.Shared.Position;
@@ -36,26 +37,24 @@ namespace PresalesApp.Web.Client.Pages
 
         private Dictionary<string, object?> GetQueryKeyValues() => new()
         {
-            [q_start] = period.Start.ToString(Helpers.Helpers.UriDateTimeFormat),
-            [q_end] = period.End.ToString(Helpers.Helpers.UriDateTimeFormat),
+            [q_start] = period.Start.ToString(Helper.UriDateTimeFormat),
+            [q_end] = period.End.ToString(Helper.UriDateTimeFormat),
             [q_keyword] = image_keyword,
         };
         #endregion
 
         protected override void OnInitialized()
         {
-            Helpers.Helpers.SetFromQueryOrStorage(value: Start, query: q_start, uri: Navigation.Uri, storage: Storage, param: ref period.Start);
-            Helpers.Helpers.SetFromQueryOrStorage(value: End, query: q_end, uri: Navigation.Uri, storage: Storage, param: ref period.End);
-            Helpers.Helpers.SetFromQueryOrStorage(value: Keyword, query: q_keyword, uri: Navigation.Uri, storage: Storage, param: ref image_keyword);
+            Helper.SetFromQueryOrStorage(value: Start, query: q_start, uri: Navigation.Uri, storage: Storage, param: ref period.Start);
+            Helper.SetFromQueryOrStorage(value: End, query: q_end, uri: Navigation.Uri, storage: Storage, param: ref period.End);
+            Helper.SetFromQueryOrStorage(value: Keyword, query: q_keyword, uri: Navigation.Uri, storage: Storage, param: ref image_keyword);
 
             Navigation.NavigateTo(Navigation.GetUriWithQueryParameters(GetQueryKeyValues()));
             RunTimer();
         }
 
         private readonly PeriodicTimer periodic_timer = new(TimeSpan.FromSeconds(1));
-        private static DateTime GetFirstDay() => new(DateTime.Now.Year, DateTime.Now.Month, 1);
-        private static DateTime GetLastDay() => GetFirstDay().AddMonths(1).AddSeconds(-1);
-        private readonly Period period = new(GetFirstDay(), GetLastDay());
+        private readonly Period period = new(new(DateTime.Now.Year, DateTime.Now.Month, 1), Enums.PeriodType.Month);
         private static string GetChartOptions() => "{\"cutout\":\"30%\",\"animation\":{\"animateScale\": true},\"plugins\":{\"tooltip\":{\"enabled\": false}}}";
 
         private readonly (byte R, byte G, byte B)[] colors = [
@@ -74,9 +73,7 @@ namespace PresalesApp.Web.Client.Pages
                 if (count > 600)
                 {
                     count = 0;
-                    period.Start = GetFirstDay();
-                    period.End = GetLastDay();
-                    await UpdateData(period);
+                    await UpdateData();
                     await UpdateImage();
                     await RedrawChart();
                     time_left = new(0, 10, 0);
@@ -86,7 +83,7 @@ namespace PresalesApp.Web.Client.Pages
             }
         }
 
-        private async Task UpdateData(Period period)
+        private async Task UpdateData()
         {
             try
             {
@@ -158,7 +155,7 @@ namespace PresalesApp.Web.Client.Pages
         {
             if (firstRender)
             {
-                await UpdateData(period);
+                await UpdateData();
                 await UpdateImage();
                 await RedrawChart();
             }
