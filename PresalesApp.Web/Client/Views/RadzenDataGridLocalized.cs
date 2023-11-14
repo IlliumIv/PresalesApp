f925a92f52c700466117f24a9a42e23805ea2298
Illium <alm.sparta.ao@gmail.com>
2023-11-14 16:48:35 +0500

    Add Roles page

---
+++ PresalesApp.Web/Client/Views/RadzenDataGridLocalized.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using Radzen.Blazor;
+
+namespace PresalesApp.Web.Client.Views;
+
+public class RadzenDataGridLocalized<TItem> : RadzenDataGrid<TItem>
+{
+    [Inject]
+    protected IStringLocalizer<App> Localization { get; set; }
+
+    protected override void OnInitialized()
+    {
+        PageSizeText = Localization["DataGridItemsPerPageText"];
+        base.OnInitialized();
+    }
+}
---
+++ PresalesApp.Web/Shared/NullableInt32.cs
@@ -0,0 +1,13 @@
+﻿namespace PresalesApp.Web.Shared;
+
+public partial class NullableInt32
+{
+    public static implicit operator int(NullableInt32? value) => ToInt(value);
+
+    public static implicit operator NullableInt32(int? value) => FromInt(value);
+
+    public static int ToInt(NullableInt32? value) => value == null ? 0 : value.Value;
+
+    public static NullableInt32 FromInt(int? value) =>
+        value is null ? new() { Null = new() } : new() { Value = (int)value };
+}
