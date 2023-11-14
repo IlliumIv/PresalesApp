6ccadb05896f7443fc0e56095dd1b00679ea6b9c
Illium <alm.sparta.ao@gmail.com>
2023-11-14 16:47:43 +0500

    Refactor

---
+++ PresalesApp.Web/Client/Services/Authorization/AuthorizationService.cs
@@ -0,0 +1,44 @@
+﻿using Blazored.LocalStorage;
+using PresalesApp.Web.Shared;
+using ApiClient = PresalesApp.Web.Shared.Api.ApiClient;
+
+namespace PresalesApp.Web.Client.Services.Authorization;
+
+public class AuthorizationService(
+    IdentityAuthenticationStateProvider provider,
+    ILocalStorageService storage,
+    ApiClient client)
+{
+    private readonly ILocalStorageService _LocalStorage = storage;
+    private readonly ApiClient _ApiClient = client;
+    private readonly IdentityAuthenticationStateProvider _StateProvider = provider;
+
+    public async Task<bool> TryRegister(RegisterRequest registerRequest)
+    {
+        var responce = await _ApiClient.RegisterAsync(registerRequest);
+        return await _ProceedLoginResponceAsync(responce);
+    }
+
+    public async Task<bool> TryLogin(LoginRequest loginRequest)
+    {
+        var responce = await _ApiClient.LoginAsync(loginRequest);
+        return await _ProceedLoginResponceAsync(responce);
+    }
+
+    public async Task Logout()
+    {
+        await _LocalStorage.RemoveItemAsync("token");
+        _StateProvider.MarkLogouted();
+    }
+
+    private async Task<bool> _ProceedLoginResponceAsync(LoginResponse loginResponse)
+    {
+        if(loginResponse.ResultCase == LoginResponse.ResultOneofCase.UserInfo)
+        {
+            await _LocalStorage.SetItemAsStringAsync("token", loginResponse.UserInfo.Token);
+            _StateProvider.MarkUserAsAuthenticated(loginResponse.UserInfo);
+            return true;
+        }
+        return loginResponse.ResultCase == LoginResponse.ResultOneofCase.Error ? throw new Exception(loginResponse.Error.Message) : false;
+    }
+}
--- PresalesApp.Web/Server/Startup/ServicesConfiguration.cs
+++ PresalesApp.Web/Server/Startup/ServicesConfiguration.cs
@@ -1,78 +1,92 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Identity;
 using PresalesApp.Database;
 using PresalesApp.Database.Entities;
 using PresalesApp.Web.Authorization;
 using PresalesApp.Web.Server.Authorization;
+using Radzen;
 using Serilog;
-using System.IdentityModel.Tokens.Jwt;
 using static PresalesApp.Database.DbController;
 
-namespace PresalesApp.Web.Server.Startup
+namespace PresalesApp.Web.Server.Startup;
+
+public static class ServicesConfiguration
 {
-    public static class ServicesConfiguration
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
     {
-        public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+        var appSettings = new AppSettings(builder.Configuration);
+        _ = builder.Services.AddScoped(s => appSettings);
+
+        DbController.Configure(appSettings);
+
+        _ = builder.Logging.ClearProviders();
+        _ = builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+
+        _ = builder.Services.AddControllersWithViews();
+        _ = builder.Services.AddRazorPages();
+        _ = builder.Services.AddScoped<DialogService>();
+        _ = builder.Services.AddScoped<NotificationService>();
+        _ = builder.Services.AddScoped<TooltipService>();
+        _ = builder.Services.AddScoped<ContextMenuService>();
+        _ = builder.Services.AddSingleton(sp =>
         {
-            var appSettings = new AppSettings(builder.Configuration);
-            builder.Services.AddScoped(s => appSettings);
+            // Get the address that the app is currently running at
+            var server = sp.GetRequiredService<IServer>();
+            var addressFeature = server.Features.Get<IServerAddressesFeature>();
+            string baseAddress = addressFeature.Addresses.First();
+            return new HttpClient { BaseAddress = new Uri(baseAddress) };
+        });
 
-            DbController.Configure(appSettings);
+        _ = builder.Services.AddGrpc();
 
-            builder.Logging.ClearProviders();
-            builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+        _ = builder.Services.AddDbContext<UsersContext>();
 
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddRazorPages();
-            builder.Services.AddGrpc();
+        _ = builder.Services.AddIdentity<User, IdentityRole>()
+            .AddEntityFrameworkStores<UsersContext>()
+            .AddDefaultTokenProviders();
 
-            builder.Services.AddDbContext<UsersContext>();
+        var tokenParams = new TokenParameters(appSettings);
+        _ = builder.Services.AddSingleton(tokenParams);
 
-            builder.Services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<UsersContext>()
-                .AddDefaultTokenProviders();
+        _ = builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = true;
+            options.UseSecurityTokenValidators = true;
+            options.SecurityTokenValidators.Add(new JwtTokenValidator(tokenParams));
+        });
 
-            var tokenParams = new TokenParameters(appSettings);
-            builder.Services.AddSingleton(tokenParams);
+        _ = builder.Services.Configure<IdentityOptions>(options =>
+        {
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+        });
 
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.RequireHttpsMetadata = true;
-                options.UseSecurityTokenValidators = true;
-                options.SecurityTokenValidators.Add(new JwtTokenValidator(tokenParams));
-            });
+        /*
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(
+                name: "cors_policy",
+                policy =>
+                {
+                    policy
+                    // .WithOrigins("http://localhost", "https://localhost", "https://127.0.0.1")
+                    // .AllowAnyMethod()
+                    // .AllowAnyOrigin()
+                    .AllowAnyHeader() // (Причина: заголовок «access-control-allow-origin» не разрешён согласно заголовку «Access-Control-Allow-Headers» из ответа CORS preflight).
+                    ;
+                });
+        });
+        //*/
 
-            builder.Services.Configure<IdentityOptions>(options =>
-            {
-                options.Password.RequiredLength = 6;
-                options.Password.RequireNonAlphanumeric = false;
-            });
+        _ = builder.Services.AddAuthorization();
 
-            /*
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy(
-                    name: "cors_policy",
-                    policy =>
-                    {
-                        policy
-                        // .WithOrigins("http://localhost", "https://localhost", "https://127.0.0.1")
-                        // .AllowAnyMethod()
-                        // .AllowAnyOrigin()
-                        .AllowAnyHeader() // (Причина: заголовок «access-control-allow-origin» не разрешён согласно заголовку «Access-Control-Allow-Headers» из ответа CORS preflight).
-                        ;
-                    });
-            });
-            //*/
-
-            builder.Services.AddAuthorization();
-
-            return builder;
-        }
+        return builder;
     }
 }
