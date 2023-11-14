227e9e1ae56ff98b5d3977e62f774b018d121f59
Illium <alm.sparta.ao@gmail.com>
2023-11-14 16:47:43 +0500

    Refactor

--- PresalesApp.Database/Entities/User.cs
+++ PresalesApp.Database/Entities/User.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-namespace PresalesApp.Database.Entities
+namespace PresalesApp.Database.Entities;
+
+public class User : IdentityUser
 {
-    public class User : IdentityUser
-    {
-        [ProtectedPersonalData]
-        public virtual string? ProfileName { get; set; }
-    }
+    [ProtectedPersonalData]
+    public virtual string? ProfileName { get; set; }
 }
