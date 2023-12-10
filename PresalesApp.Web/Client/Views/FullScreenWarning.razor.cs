0a3a9bc2fb332a7d21624b6657c8ff349ed529be
Illium <alm.sparta.ao@gmail.com>
2023-12-10 12:42:04 +0500

    Move glithes warning to separate class

---
+++ PresalesApp.Web/Client/Views/FullScreenWarning.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PresalesApp.Web.Client.Views;
+
+partial class FullScreenWarning
+{
+    [Parameter]
+    public string Message { get; set; } = "Нужно проветрить!";
+
+    [Parameter]
+    public bool IsGlitched { get; set; } = false;
+
+    private string _Display = "none";
+
+    private TimeSpan _TimeLeft = new(0, 10, 0);
+}
