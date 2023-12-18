4d260e08d7df0b3bec68814194bfd44192e9cc88
Illium <alm.sparta.ao@gmail.com>
2023-12-18 20:39:21 +0500

    App.Database refactor

--- PresalesApp.Database/Enums/ProjectStatus.cs
+++ PresalesApp.Database/Enums/ProjectStatus.cs
@@ -2,18 +2,20 @@
 using PresalesApp.Database.Helpers;
 using System.Runtime.Serialization;
 
-namespace PresalesApp.Database.Enums
+namespace PresalesApp.Database.Enums;
+
+[JsonConverter(typeof(SafeStringEnumConverter), Unknown)]
+public enum ProjectStatus
 {
-    [JsonConverter(typeof(SafeStringEnumConverter), Unknown)]
-    public enum ProjectStatus
-    {
-        [EnumMember(Value = "Неопределён")]
-        Unknown,
-        [EnumMember(Value = "В работе")]
-        WorkInProgress,
-        [EnumMember(Value = "Выиграна")]
-        Won,
-        [EnumMember(Value = "Проиграна")]
-        Loss
-    }
+    [EnumMember(Value = "Неопределён")]
+    Unknown,
+
+    [EnumMember(Value = "В работе")]
+    WorkInProgress,
+
+    [EnumMember(Value = "Выиграна")]
+    Won,
+
+    [EnumMember(Value = "Проиграна")]
+    Loss
 }
--- PresalesApp.Database/Migrations/20220930054249_UpdateInvoice.cs
+++ PresalesApp.Database/Migrations/20220930054249_UpdateInvoice.cs
@@ -2,34 +2,33 @@
 
 #nullable disable
 
-namespace PresalesApp.Database.Migrations
+namespace PresalesApp.Database.Migrations;
+
+public partial class UpdateInvoice : Migration
 {
-    public partial class UpdateInvoice : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<DateTime>(
-                name: "LastPay",
-                table: "Invoices",
-                type: "timestamp with time zone",
-                nullable: true);
+        migrationBuilder.AddColumn<DateTime>(
+            name: "LastPay",
+            table: "Invoices",
+            type: "timestamp with time zone",
+            nullable: true);
 
-            migrationBuilder.AddColumn<DateTime>(
-                name: "LastShipment",
-                table: "Invoices",
-                type: "timestamp with time zone",
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<DateTime>(
+            name: "LastShipment",
+            table: "Invoices",
+            type: "timestamp with time zone",
+            nullable: true);
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "LastPay",
-                table: "Invoices");
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "LastPay",
+            table: "Invoices");
 
-            migrationBuilder.DropColumn(
-                name: "LastShipment",
-                table: "Invoices");
-        }
+        migrationBuilder.DropColumn(
+            name: "LastShipment",
+            table: "Invoices");
     }
 }
