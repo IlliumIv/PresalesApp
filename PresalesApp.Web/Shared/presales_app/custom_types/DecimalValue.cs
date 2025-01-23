28b02cdda883aedd41cc43e6b2e1b13b588e5267
Illium <alm.sparta.ao@gmail.com>
2025-01-23 17:02:41 +0500

    Servrices Rework fix 1

---
+++ PresalesApp.Web/Shared/presales_app/custom_types/DecimalValue.cs
@@ -0,0 +1,33 @@
+﻿namespace PresalesApp.CustomTypes;
+
+public partial class DecimalValue : IComparable
+{
+    private const decimal _NanoFactor = 1_000_000_000;
+
+    public DecimalValue(long units, int nanos)
+    {
+        Units = units;
+        Nanos = nanos;
+    }
+
+    public static implicit operator decimal(DecimalValue? decimalValue) => ToDecimal(decimalValue);
+
+    public static implicit operator DecimalValue(decimal value) => FromDecimal(value);
+
+    public static decimal ToDecimal(DecimalValue? decimalValue) => decimalValue == null
+        ? 0
+        : decimalValue.Units + (decimalValue.Nanos / _NanoFactor);
+
+    public static DecimalValue FromDecimal(decimal value)
+    {
+        var units = decimal.ToInt64(value);
+        var nanos = decimal.ToInt32((value - units) * _NanoFactor);
+        return new DecimalValue(units, nanos);
+    }
+
+    public int CompareTo(object? value) => value is null
+            ? 1
+            : value is not DecimalValue dv
+                ? throw new ArgumentException($"Argument must be {GetType().Name}")
+                : decimal.Compare(this, dv);
+}
